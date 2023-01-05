620c6390c7aacf5eb5a00c145c068c320b4f2a90
Jepril Achapero <jeprilachapero@gmail.com>
2023-01-05 23:52:24 +0800

    update

--- resources/js/components/views/errors/Error404.js
+++ resources/js/components/views/errors/Error404.js
@@ -2,28 +2,33 @@ import React from "react";
 import { Result, Button } from "antd";
 import { useHistory } from "react-router";
 const Error404 = () => {
-	let history = useHistory();
-	// useEffect(() => {
-	//     // history.push("/?redirect=" + window.location.href);
-	//     console.log('history', history)
-	//     return () => {};
-	// }, []);
-	return (
-		<Result
-			status="404"
-			title="404"
-			subTitle="Sorry, something went wrong."
-			extra={
-				<Button
-					type="primary"
-					onClick={(e) => history.push("/?redirect=" + window.location.href)}
-					className="btn-primary-default invert"
-				>
-					Back Home
-				</Button>
-			}
-		/>
-	);
+    let history = useHistory();
+    useEffect(() => {
+        localStorage.removeItem("userdata");
+        localStorage.removeItem("token");
+        localStorage.removeItem("userdata_admin");
+        localStorage.removeItem("viewas");
+        return () => {};
+    }, []);
+    return (
+        <Result
+            status="404"
+            title="404"
+            subTitle="Sorry, something went wrong."
+            extra={
+                <Button
+                    type="primary"
+                    onClick={(e) => {
+                        window.location.href =
+                            "/?redirect=" + window.location.href;
+                    }}
+                    className="btn-primary-default invert"
+                >
+                    Back Home
+                </Button>
+            }
+        />
+    );
 };
 
 export default Error404;
